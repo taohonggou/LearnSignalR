e6a4d259821348474888b2d1ce9e09ba41f28604
chenliang <taohonggou@outlook.com>
2020-07-04 11:09:16 +0800

    新添加了项目

---
+++ SignalRMvcChat/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            var dd = Context.ConnectionId;
+            
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
